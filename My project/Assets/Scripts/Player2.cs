0afa42dfceafc02a9ede303428c5294a7d9eda28
24lathonas <122646576+24lathonas@users.noreply.github.com>
2023-03-03 15:13:32 -0600

    Update Player2.cs

---
+++ My project/Assets/Scripts/Player2.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+    private float speed = 12.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+            if(Input.GetKey(KeyCode.A))
+                transform.Rotate(0, -90 * Time.deltaTime, 0);
+            if(Input.GetKey(KeyCode.D))
+                transform.Rotate(0, 90 * Time.deltaTime, 0);
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+            if(Input.GetKey(KeyCode.A))
+                transform.Rotate(0, -90 * Time.deltaTime, 0);
+            if(Input.GetKey(KeyCode.D))
+                transform.Rotate(0, 90 * Time.deltaTime, 0);
+        }
+    }
+}
