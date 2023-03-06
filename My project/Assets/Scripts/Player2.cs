ec3111fd9f934b18ee23d91e5a06d92a26243cd7
26haufedr <122646585+26haufedr@users.noreply.github.com>
2023-03-06 15:25:17 -0600

    Update

--- My project/Assets/Scripts/Player2.cs
+++ My project/Assets/Scripts/Player2.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player2 : MonoBehaviour
 {
-    private float speed = 12.0f;
+    public float speed = 12.0f;
 
     // Start is called before the first frame update
     void Start()
